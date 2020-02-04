4234c4963af080bf464c188a9f602cf17c51a227
Johnathan Hall <jhnhall94@gmail.com>
2020-02-03 21:51:16 -0500

    Explicit loops perform as expected. Single line in progress

--- Assignment4.m
+++ Assignment4.m
@@ -2,22 +2,48 @@
 
 function main()
 
-item   = [ 1  2  3 4 5 6  7]
-profit = [30 14 16 9];
-weight = [ 6  3  4 2];
+item   = [ 1  2  3 4 5 6  7];
+profit = [9 14 16 30];
+weight = [ 2 3 4 6];
 maxWeight = 10;
 
-% 2D array of size 4x4 to hold all solutions
-k = zeros(length(profit),maxWeight);
+% 2D array to hold all solutions
+k = zeros(length(profit)+1,maxWeight+1);
 
 % Start at index 2 because the first row and first column need to be zeros
-for j = 2:length(profit)
-	for w = 2:maxWeight
-		k(w,j) = k(j-1,w);
-		k(w,j) = max([k(weight(w)-weight(j),j-1) + profit(j),k(w,j-1) ]);
-
+for i = 2:length(profit)+1
+    % j is the current small knapsack max weight
+	for j = 2:maxWeight+1
+        % Assign the weight from the row above (the previous profit). Build
+        % on previous profits to inform the current small knapsack
+		k(i,j) = k(i-1,j);
+        % If the weight being tested is larger than the current small
+        % knapsack, skip it
+        if(weight(i-1)+1 > j)
+            continue
+        end
+        % The candidate is the value of the previous small knapsack plus
+        % this small knapsack
+        candidate = k(i-1,j-weight(i-1)) + profit(i-1);
+        % If this candidate profit is greater than the previous profit,
+        % update the weight array
+        if(candidate > k(i,j))
+    		k(i,j) = candidate;
+        end
 	end
 end
+
+% 2D array to hold all solutions
+m = zeros(length(profit)+1,maxWeight+1);
+
+% Start at index 2 because the first row and first column need to be zeros
+for i = 2:length(profit)+1
+    % j is the current small knapsack max weight
+    for j = 2:maxWeight+1
+        m(i,j) = max([m(i-1,profit(j)-weight(i-1)) + profit(i-1),m(i-1,j)+1]);
+    end
+end
+
 
 end
 
